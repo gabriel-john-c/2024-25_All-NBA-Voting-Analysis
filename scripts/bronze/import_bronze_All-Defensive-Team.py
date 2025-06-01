f9d33b239b4362afe08848a486c4019e833f773b
Gabriel-John Cunanan <gabriel.john.cunanan@gmail.com>
2025-06-01 15:01:28 -0700

    add remainder of bronze ingest

---
+++ scripts/bronze/import_bronze_All-Defensive-Team.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+#grab working directory
+current_dir = os.path.dirname(__file__)
+
+#path for csv imput file
+csv_path = os.path.join(current_dir, '..', '..', 'data', 'bronze', '2024-25-Kia-NBA-All-Defensive-Team-Voter-Selections.csv')
+csv_path = os.path.abspath(csv_path)
+
+#create pandas df
+df = pd.read_csv(csv_path)
+
+print(df)

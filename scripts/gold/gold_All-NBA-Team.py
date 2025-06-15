83152a15d512a9b9c6f9dd959325143905bb50f8
Gabriel-John Cunanan <gabriel.john.cunanan@gmail.com>
2025-06-15 10:29:04 -0700

    update gold layer for all nba team processing

--- scripts/gold/gold_All-NBA-Team.py
+++ scripts/gold/gold_All-NBA-Team.py
@@ -34,7 +34,6 @@ player_team_counts['team1-votes'] = player_team_counts['Team1_1'] + player_team_
 player_team_counts['team2-votes'] = player_team_counts['Team2_1'] + player_team_counts['Team2_2'] + player_team_counts['Team2_3'] + player_team_counts['Team2_4'] + player_team_counts['Team2_5']
 player_team_counts['team3-votes'] = player_team_counts['Team3_1'] + player_team_counts['Team3_2'] + player_team_counts['Team3_3'] + player_team_counts['Team3_4'] + player_team_counts['Team3_5']
 
-
 #build points columns and index
 points_columns = ['Team1_1', 'Team1_2', 'Team1_3', 'Team1_4', 'Team1_5', 'Team2_1', 'Team2_2', 'Team2_3', 'Team2_4', 'Team2_5', 'Team3_1', 'Team3_2', 'Team3_3', 'Team3_4', 'Team3_5']
 points_index = player_team_counts.index
@@ -61,7 +60,46 @@ df_points_calc = pd.DataFrame(
 #sum across columns, store in new column
 df_points_calc['total-pts'] = df_points_calc[list(df_points_calc.columns)].sum(axis=1)
 
-#.drop(columns=points_columns)
-print(team_vote_categories)
-print(player_team_counts)
-print (df_points_calc)
+#average vote score - vote sum divided by total number of voters (100)
+df_points_calc['avg-vote-score'] = (df_points_calc['total-pts']) / len(voters)
+
+#create df for voter skew, voters as index, columns for each player
+df_voter_skew = pd.DataFrame(index=voters,columns=players)
+
+#loop through our new skew df
+for i, row in df_voter_skew.iterrows():
+    skew_voter = i #store the voter name
+    for column, value in row.items():
+      skew_player = column #store the player name
+
+      #lookup the average score of the player by referencing the df_points_calc dataframe
+      skew_player_avg_score = df_points_calc.loc[df_points_calc.index == skew_player,'avg-vote-score'].values[0]
+      
+      #calculate the voters value assignment, get the team assignment (team1 / team2 / team3)
+      skew_voter_player_team = df[df['voter'] == skew_voter].apply(lambda row: row[row == skew_player], axis=1).columns.values
+      
+      #calculate the points depending on the team
+      if skew_voter_player_team.size > 0: #if the array has items - some players are omitted from voters ballots resulting in empty value here
+        if skew_voter_player_team[0].startswith('Team1'):
+          skew_voter_player_value = 5
+        elif skew_voter_player_team[0].startswith('Team2'):
+          skew_voter_player_value = 3
+        elif skew_voter_player_team[0].startswith('Team3'):
+          skew_voter_player_value = 1
+      else:
+        skew_voter_player_value = 0 #default value for ommissions
+      
+      #calculate the skew for this voter + player combination. take assigned value - avg value to determine skew. 0 is exact, negative value is undervalueing/unconventional, positive value is overvalue/conventional
+      skew = round(skew_voter_player_value - skew_player_avg_score, 2)
+      print(f"Player: {skew_player} has an avg score of: {skew_player_avg_score} points")
+      print(f"Voter skew {skew_voter} for player {skew_player} is {skew} points")
+      #update/store skew in cell for df_voter_skew dataframe
+      df_voter_skew.at[i, column] = skew
+
+#print(team_vote_categories)
+#print(player_team_counts)
+#print(df_points_calc)
+print(df_voter_skew)
+#print(df[df['voter'] == 'David Aldridge'])
+#print(df[df['voter'] == 'David Aldridge'].apply(lambda row: row[row == 'Giannis Antetokounmpo, Milwaukee'], axis=1).columns.values[0])
+#print(df_points_calc.loc[df_points_calc.index == 'Giannis Antetokounmpo, Milwaukee','avg-vote-score'])
